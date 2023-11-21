ae3fea5efc5350c9b5bdf0affde6e84875b24949
Kuang-Yu <kuangyu.nien@somnics.com>
2023-11-21 14:51:35 +0800

    Initial Commit

---
+++ GetLocationByPlaceName/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GetLocationByPlaceName
+//
+//  Created by SomnicsAndrew on 2023/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
